592db8f0ec7266ee5992f51ba9daa98ac4d4a745
Ahmd-Sadka <flookaty@gmail.com>
2023-11-13 16:57:00 -0800

    task 0_updated

--- _printf.c
+++ _printf.c
@@ -13,6 +13,9 @@ int _printf(const char *format, ...)
 
 	unsigned int i = 0, count = 0, str_count;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
