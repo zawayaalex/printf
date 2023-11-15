db6233e1cbb90ad09c940ec997c6248911e59da7
“zawayaalex” <zawayaalex@gmail.com>
2023-11-14 16:09:54 -0800

    print i & d

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 {
 int counted = 0, i = 0, a = 0;
 va_list argus;
-fn_params find_sp[] = {{pr_char, "%c"}, {pr_str, "%s"}, {pr_perc, "%%"}};
+ fn_params find_sp[] = {{pr_char, "%c"}, {pr_str, "%s"}, {pr_perc, "%%"}, {pr_i_d, "%i"}, {pr_i_d, "%d"}};
 if (format == NULL || (format[0] == '%' && !format[1]))
 return (-1);
 if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -21,7 +21,7 @@ look:
 while (format[i] != '\0')
 {
 a = 0;
-while (a < 3)
+while (a < 5)
 {
 if (format[i] == find_sp[a].ptr_sp[0] && format[i + 1] == find_sp[a].ptr_sp[1])
 {
